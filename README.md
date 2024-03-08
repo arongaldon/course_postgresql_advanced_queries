0538d04a1ab4183edfcc6cf52c7d9ec942a33a1c
Aron Galdon Gines <41184401+arongaldon@users.noreply.github.com>
2024-03-08 12:38:29 +0100

    First chapter and first challenge

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 
 My own docker compose environment in order to follow this training: [postgresql-advanced-queries course on LinkedIn Learning](https://www.linkedin.com/learning/postgresql-advanced-queries).
 
-```sh
+```Shell
 docker compose up -d --build
 ```
+
+## What was new to me
+
+### c1
+
+```SQL
+GROUP BY CUBE (/* col, ... */)
+GROUP BY ROLLUP (/* col, ... */)
+bool_and(/* col */)
+bool_or(/* col */)
+stddev_pop(/* col */)
+stddev_samp(/* col */)
+var_pop(/* col */)
+var_samp(/* col */)
+/* aggregate function */ FILTER (WHERE /* condition */) AS /* "name" */
+´´´
---
+++ challenge01_aron.sql
@@ -0,0 +1,61 @@
+-- challenge01_aron.sql
+-- Write two queries that explore the Two Trees Olive Oil order data.
+/* From sales.orders, find the number of orders placed per month for
+each customer; use an aggregate filter and remember that dates use
+single quotes. */
+CREATE
+OR REPLACE VIEW orders_per_month_and_customer AS
+SELECT
+    DATE(DATE_TRUNC ('month', "order_date")) AS "Order month",
+    customer_id,
+    COUNT(*) AS "Number of orders"
+FROM
+    sales.orders
+GROUP BY
+    "Order month",
+    customer_id;
+
+SELECT
+    "Order month",
+    customer_id,
+    "Number of orders"
+FROM
+    orders_per_month_and_customer
+ORDER BY
+    customer_id;
+
+SELECT
+    customer_id,
+    "Number of orders"
+FROM
+    orders_per_month_and_customer
+WHERE
+    "Order month" = '2021-03-01'
+ORDER BY
+    customer_id;
+
+SELECT
+    customer_id,
+    "Number of orders"
+FROM
+    orders_per_month_and_customer
+WHERE
+    "Order month" = '2021-04-01'
+ORDER BY
+    customer_id;
+
+SELECT
+    customer_id,
+    SUM("Number of orders") FILTER (
+        WHERE
+            "Order month" BETWEEN '2021-01-01' AND '2021-12-31'
+    )
+FROM
+    orders_per_month_and_customer
+GROUP BY
+    customer_id
+ORDER BY
+    customer_id;
+
+/* From sales.order_lines, find the total quantity of each product
+sold; use the sum() aggregate function to add up the product quantities. */
